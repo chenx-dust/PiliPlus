861602fd376013086c7585e5299c4a4342da4b36
Chenx Dust <chenx_dust@outlook.com>
2026-02-13 21:07:19 +0800

    chore: default aaudio

--- lib/plugin/pl_player/models/audio_output_type.dart
+++ lib/plugin/pl_player/models/audio_output_type.dart
@@ -1,9 +1,9 @@
 import 'package:PiliPlus/models/common/enum_with_label.dart';
 
 enum AudioOutput implements EnumWithLabel {
-  opensles('OpenSL ES'),
   aaudio('AAudio'),
-  audiotrack('AudioTrack')
+  audiotrack('AudioTrack'),
+  opensles('OpenSL ES')
   ;
 
   static final defaultValue = values.map((e) => e.name).join(',');
